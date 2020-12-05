0ebf230b01c4fdcf2d7f59918c63acf349f7b89f
Leo Stelon <leostelon17@gmail.com>
2020-12-05 11:59:45 +0530

    forgotpass include file in index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var cors = require('cors')
 const express=require('express')
 const User=require('./routers/user')
 const Message = require('./routers/message')
+const ForgotPassword = require('./routers/forgotpass')
 
 const port=process.env.PORT || 3000
 const app=express()
@@ -14,13 +15,16 @@ const server = http.createServer(app);
 require('./ws/ws')(server)
 
 const publicDirectoryPath = path.join(__dirname,'../public')
+const cdnDirectoryPath = path.join(__dirname,'../cdn')
 
 app.use(express.static(publicDirectoryPath));
+app.use(express.static(cdnDirectoryPath));
 
 app.use(cors())
 app.use(express.json())
 app.use(User)
 app.use(Message)
+app.use(ForgotPassword)
 
 app.get('/test',(req,res)=>{
     res.send({message:'Server OK.'})
