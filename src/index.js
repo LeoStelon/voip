a1b926336a9cf61aaaef7227ca9b394929725cb2
Leo Stelon <leostelon17@gmail.com>
2020-12-05 21:49:44 +0530

    websocket test 2

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ const port=process.env.PORT || 3000
 const app=express()
 
 const http = require('http');
+const auth = require('./middlewares/auth')
 const server = http.createServer(app);
 //Websocket Server
-require('./ws/ws')(server)
+// require('./ws/ws')(server)
 
 const publicDirectoryPath = path.join(__dirname,'../public')
 const cdnDirectoryPath = path.join(__dirname,'../cdn')
@@ -29,6 +30,31 @@ app.use(ForgotPassword)
 app.get('/test',(req,res)=>{
     res.send({message:'Server OK.'})
 })
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ noServer: true });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    ws.send(message)
+  });
+
+  ws.send('Connected');
+});
+
+const url = require('url');
+server.on('upgrade', function upgrade(request, socket, head) {
+    const pathname = url.parse(request.url).pathname;
+  
+    if (pathname === '/test') {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            wss.emit('connection', ws, request);
+        });
+    } else {
+        // Websocket Server
+        require('./ws/ws')(server)
+    }
+});
 
 server.listen(port,()=>{
     console.log('Server listening on port '+port)
